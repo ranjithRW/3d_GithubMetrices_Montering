1b82ab3e994374e945547a2a2eda5a37bef7d695
Ranjith.T <121870537+ranjiththangavel2000@users.noreply.github.com>
2025-05-13 15:29:57 +0530

    butterfly 3d reender

---
+++ node_modules/@theatre/r3f/dist/index.d.ts
@@ -0,0 +1,6 @@
+export { default as editable } from './main/editable';
+export type { EditableState, BindFunction } from './main/store';
+export { default as SheetProvider, useCurrentSheet } from './main/SheetProvider';
+export { refreshSnapshot } from './main/utils';
+export { default as RefreshSnapshot } from './main/RefreshSnapshot';
+//# sourceMappingURL=index.d.ts.map
---
+++ node_modules/@theatre/r3f/dist/main/utils.d.ts
@@ -0,0 +1,4 @@
+import type { ISheet } from '@theatre/core';
+export declare const refreshSnapshot: () => void;
+export declare const makeStoreKey: (sheet: ISheet, name: string) => string;
+//# sourceMappingURL=utils.d.ts.map
