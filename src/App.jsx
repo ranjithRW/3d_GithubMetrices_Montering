feb42e037ae320675e1e41f89afc1488bf6f731e
Ranjith.T <121870537+ranjiththangavel2000@users.noreply.github.com>
2025-05-19 15:29:21 +0530

    refactor: remove unused components and clean up routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 // app.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router, Routes, Route
-import ModelPage from './Pages/Modelpage';
-import Mainpage from './Pages/Mainpage';
-import DeveloperResorce from './Pages/developerResource'
+
 import MergeCode from './Pages/mergecode'
 
 function App() {
   return (
-    <Router>  {/* Wrap everything in Router */}
-      <Routes>  {/* Use Routes to define routes */}
-        <Route path="/" element={<Mainpage />} /> {/* Define the root path to Mainpage */}
-        <Route path="/modelpage" element={<ModelPage />} /> {/* Define the /modelpage path to ModelPage */}
-        {/* <Route path="/developerResource" element={<DeveloperResorce />} />  */}
-                <Route path="/developerResource" element={<MergeCode />} /> 
-
-      </Routes>
-    </Router>
+  <div>
+    <MergeCode />
+  </div>
   );
 }
 
